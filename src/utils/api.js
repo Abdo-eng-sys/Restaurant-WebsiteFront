ff086e24ada32df8a36241cd464db6626e7e03af
Abd Ul-Rahman <=>
2026-07-03 17:55:47 +0300

    Update Backend URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://restaurant-websiteback-production.up.railway.app/",
+  baseURL: process.env.REACT_APP_API_URL || "https://web-production-0d124.up.railway.app/",
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
